6b0f3863eda3f29e599088161e771a4f73fc2964
joao francisco <jffilipe@hotmail.com>
2023-04-19 12:31:57 +0100

    Add project files.

---
+++ ePerfumes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ePerfumes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
