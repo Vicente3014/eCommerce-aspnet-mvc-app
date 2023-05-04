3aaa700d82debc08e18f5ea34df6f1636bb5ec7b
joao francisco <jffilipe@hotmail.com>
2023-05-04 19:23:36 +0100

    Create Perfume/Edit Perfume Beginning

--- ePerfumes/Models/Perfume.cs
+++ ePerfumes/Models/Perfume.cs
@@ -8,16 +8,25 @@ namespace ePerfumes.Models
     {
         [Key]
         public int Perfume_ID { get; set; }
+        [Required(ErrorMessage ="Por favor introduza o nome do perfume")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "O Nome do Perfume deve ter entre 5 e 50 caracteres")]
         [Display(Name = "Nome do Perfume")]
         public string Perfume_Name { get; set; }
+        [Required(ErrorMessage ="Por favor introduza o URL da fotografia do perfume")]
         [Display(Name = "Foto do Perfume")]
         public string Perfume_Pic_URL { get; set;}
+        [Required(ErrorMessage ="Por favor introduza o tamanho do perfume")]
         [Display(Name = "Tamanho em ml")]
+        [Range(50,150)]
         public int Tamanho { get; set; }
         [Display(Name = "Preço(€)")]
+        [Required(ErrorMessage ="Por favor introduza o preço do perfume")]
+        [Range(0.0,Double.PositiveInfinity)]
         public double Price { get; set; }
+        [Required(ErrorMessage ="Por favor escolha o género do perfume")]
         [Display(Name = "Para:")]
         public PerfumeType PerfumeType { get; set; }
+        [Required(ErrorMessage ="Por favor escolha um tipo de perfume")]
         [Display(Name = "Tipo de Perfume")]
         public PerfumeVersion PerfumeVersion { get; set; }
 
